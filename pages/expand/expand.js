03ecc6066a964ea58aa34f3226ca854a08be6f2e
aw <3193249207@qq.com>
2025-10-29 15:13:21 +0800

    推荐人列表骨架屏自定义

--- pages/expand/expand.js
+++ pages/expand/expand.js
@@ -85,10 +85,6 @@ function writeListCache(payload) {
 
 Page({
   data: {
-    rowColsAvater: [{ size: '96rpx', type: 'circle' }],
-    rowColsImage: [{ size: '96rpx', type: 'rect' }],
-    rowColsContent: [{ width: '50%' }, { width: '100%' }],
-
     currentTab: 'recommend',
     scrollTop: { recommend: 0, potential: 0 },
 
